79bfde752a1846627691b08b48b5766a917f7719
Shado <94776476+shevtsov-dmitry@users.noreply.github.com>
2023-02-23 15:05:54 +0300

    Добавил back-end часть

---
+++ air-pollution-map-project-backend/src/main/java/com/example/airpollutionmapprojectbackend/AirPollutionMapProjectBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.airpollutionmapprojectbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirPollutionMapProjectBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AirPollutionMapProjectBackendApplication.class, args);
+    }
+
+}
