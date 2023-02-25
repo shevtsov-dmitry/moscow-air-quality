51bd266784921acb03e31aa88fca40f0169d4d45
shado <shadowstakeus28@gmail.com>
2023-02-26 00:36:51 +0300

    Настроил начальные зависимости для подключения в базу данных и добавил серверную базовую страницу.

--- air-pollution-map-project-backend/src/main/java/com/example/airpollutionmapprojectbackend/AirPollutionMapProjectBackendApplication.java
+++ air-pollution-map-project-backend/src/main/java/com/example/airpollutionmapprojectbackend/AirPollutionMapProjectBackendApplication.java
@@ -2,12 +2,18 @@ package com.example.airpollutionmapprojectbackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+    @RestController
 public class AirPollutionMapProjectBackendApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(AirPollutionMapProjectBackendApplication.class, args);
     }
-
+    @GetMapping(value = "/", produces = MediaType.TEXT_HTML_VALUE)
+    public static String mainPage(){
+        return "static/index.html";
+    }
 }
