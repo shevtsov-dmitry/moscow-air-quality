9d173066b4d3894ae523a441688a7feaaf2855a8
shado <shadowstakeus28@gmail.com>
2023-02-27 19:54:45 +0300

    сделать реорганизацию в файлах добавил странцу-посредника для авторизации

--- air-pollution-map-project-frontend/main.js
+++ air-pollution-map-project-frontend/main.js
@@ -1,17 +1,53 @@
 import './style.css';
-import {Map, View} from 'ol';
-import TileLayer from 'ol/layer/Tile';
-import OSM from 'ol/source/OSM';
+import Draw from 'ol/interaction/Draw.js';
+import Map from 'ol/Map.js';
+import View from 'ol/View.js';
+import {OSM, Vector as VectorSource} from 'ol/source.js';
+import {Tile as TileLayer, Vector as VectorLayer} from 'ol/layer.js';
+
+const raster = new TileLayer({
+  source: new OSM(),
+});
+
+const source = new VectorSource({wrapX: false});
+
+const vector = new VectorLayer({
+  source: source,
+});
 
 const map = new Map({
+  layers: [raster, vector],
   target: 'map',
-  layers: [
-    new TileLayer({
-      source: new OSM()
-    })
-  ],
   view: new View({
-    center: [0, 0],
-    zoom: 2
-  })
+    center: [-11000000, 4600000],
+    zoom: 4,
+  }),
 });
+
+const typeSelect = document.getElementById('type');
+
+let draw; // global so we can remove it later
+function addInteraction() {
+  const value = typeSelect.value;
+  if (value !== 'None') {
+    draw = new Draw({
+      source: source,
+      type: typeSelect.value,
+    });
+    map.addInteraction(draw);
+  }
+}
+
+/**
+ * Handle change event.
+ */
+typeSelect.onchange = function () {
+  map.removeInteraction(draw);
+  addInteraction();
+};
+
+document.getElementById('undo').addEventListener('click', function () {
+  draw.removeLastPoint();
+});
+
+addInteraction();
