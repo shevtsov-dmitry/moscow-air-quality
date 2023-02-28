47ebc1348110e20e028ccbb6f82f2bbd7336bde4
shado <shadowstakeus28@gmail.com>
2023-02-28 23:46:26 +0300

    Остановил работу на получении запроса из вебсайта и исключении TemplateInputException

--- air-pollution-map-project-backend/src/main/java/com/example/airpollutionmapprojectbackend/AirPollutionMapProjectBackendApplication.java
+++ air-pollution-map-project-backend/src/main/java/com/example/airpollutionmapprojectbackend/AirPollutionMapProjectBackendApplication.java
@@ -4,10 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 @SpringBootApplication
 @Controller
@@ -15,6 +12,7 @@ public class AirPollutionMapProjectBackendApplication {
     public static void main(String[] args) {
         SpringApplication.run(AirPollutionMapProjectBackendApplication.class, args);
     }
+    @CrossOrigin(origins = "http://localhost:5173")
     @RequestMapping("/")
     public String showMainPageHTML(){
         return "dynamicOutput";
--- air-pollution-map-project-frontend/authorization_script.js
+++ air-pollution-map-project-frontend/authorization_script.js
@@ -1,18 +1,18 @@
-const form = document.querySelector(".login-form");
+const form = document.querySelector("#form-with-data");
 form.addEventListener("submit", (event)=>{
     // блокирую стандартное выполнение формы
     event.preventDefault();
-    
-const formData = new formData(form);
+const formData = new FormData(form);
 const url = "http://localhost:8080/";
     // отправляю данные на сервер для проверки действительности
 
-fetch(url,{
-    method:"POST",
-    body: formData
-})
-.then(response=> response.json())
-.then(data => console.log(data))
-.catch(console.log("didn't send"));
+    fetch(url,{
+        method:"POST",
+        body: formData
+    })
+    .then(response=> response.json())
+    .then(data => console.log(data))
+    .catch(error =>console.log("didn't send" +"\n" + error));
 
+    console.log(formData)
 })
