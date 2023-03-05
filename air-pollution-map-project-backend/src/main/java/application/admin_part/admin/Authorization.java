62909088c41db9bd30edc304732feea5bd5cb11d
shado <shadowstakeus28@gmail.com>
2023-03-05 17:06:39 +0300

    Создал класс Admin и вспомогательные классы для верификации логина и пароля

--- air-pollution-map-project-backend/src/main/java/application/admin_part/admin/Authorization.java
+++ air-pollution-map-project-backend/src/main/java/application/admin_part/admin/Authorization.java
@@ -1,13 +1,16 @@
-package application.admin_part;
+package application.admin_part.admin;
 
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class Authorization {
+abstract class Authorization {
     @GetMapping(value = "/admin", produces = MediaType.TEXT_HTML_VALUE)
     public String showServerHtmlPage(){
-        return "static/index.html";
+        return "templates/index.html";
+    }
+    public boolean verify() {
+        return false;
     }
 }
---
+++ air-pollution-map-project-backend/src/main/java/application/admin_part/admin/VerifyAdmin.java
@@ -0,0 +1,12 @@
+package application.admin_part.admin;
+
+import application.admin_part.admin.Authorization;
+
+public class VerifyAdmin extends Authorization {
+    Admin admin = new Admin();
+    @Override
+    public boolean verify() {
+        return super.verify();
+    }
+
+}
