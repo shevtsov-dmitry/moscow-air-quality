8267cd90737069b5d2a5f1e820f3abdff175fb5e
shado <shadowstakeus28@gmail.com>
2023-03-05 17:06:39 +0300

    Создал класс Admin и вспомогательные классы для верификации логина и пароля

--- air-pollution-map-project-backend/src/main/java/application/admin_part/admin/Admin.java
+++ air-pollution-map-project-backend/src/main/java/application/admin_part/admin/Admin.java
@@ -1,4 +1,53 @@
 package application.admin_part.admin;
 
 public class Admin {
+    private long id;
+    private String login;
+    private String password; // in the future will try to use char[] instead of String
+    public Admin() {
+    }
+
+    public Admin(long id, String login, String password) {
+        this.id = id;
+        this.login = login;
+        this.password = password;
+    }
+
+    public Admin(String login, String password) {
+        this.login = login;
+        this.password = password;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return "Admin{" +
+                "id=" + id +
+                ", login='" + login + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
 }
--- air-pollution-map-project-backend/src/main/java/com/example/airpollutionmapprojectbackend/AirPollutionMapProjectBackendApplication.java
+++ air-pollution-map-project-backend/src/main/java/com/example/airpollutionmapprojectbackend/AirPollutionMapProjectBackendApplication.java
@@ -3,17 +3,18 @@ package com.example.airpollutionmapprojectbackend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @SpringBootApplication
-    @RestController
+@RestController
+@CrossOrigin(origins = "http://localhost:5173", methods = { RequestMethod.GET, RequestMethod.POST })
 public class AirPollutionMapProjectBackendApplication {
     public static void main(String[] args) {
         SpringApplication.run(AirPollutionMapProjectBackendApplication.class, args);
     }
     @GetMapping(value = "/", produces = MediaType.TEXT_HTML_VALUE)
     public static String mainPage(){
-        return "static/index.html";
+        return "templates/index.html";
     }
+
 }
