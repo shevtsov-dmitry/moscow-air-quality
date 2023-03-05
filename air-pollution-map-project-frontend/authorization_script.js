cfb3bf7876c08377d2369b342eb8a59534e7c706
shado <shadowstakeus28@gmail.com>
2023-03-05 17:32:50 +0300

    Переписал js функцию для отправки JSON формы на сервер

--- air-pollution-map-project-frontend/authorization_script.js
+++ air-pollution-map-project-frontend/authorization_script.js
@@ -1,18 +1,32 @@
 const form = document.querySelector("#form-with-data");
+// Функция: отправить форму на обработку на сервер
 form.addEventListener("submit", (event)=>{
     // блокирую стандартное выполнение формы
     event.preventDefault();
-const formData = new FormData(form);
-const url = "http://localhost:8080/";
+    const login = form.querySelector("#username").value;
+    const password = form.querySelector("#password").value;
+    const formData = {
+        login: login,
+        password: password
+    }
+    const url = "http://localhost:8080/";
     // отправляю данные на сервер для проверки действительности
 
     fetch(url,{
         method:"POST",
-        body: formData
+        headers:{
+            'Content-Type':'application/json'
+        },
+        body: JSON.stringify(formData)
     })
-    .then(response=> response.json())
-    .then(data => console.log(data))
-    .catch(error =>console.log("didn't send" +"\n" + error));
+    .then(response =>{
+        if(!response.ok){
+            throw new Error('Network response was not completed');
+        }
+        else return response.json();
+    })
+    .then(data => console.log("Response data from server" + data))
+    .catch(error =>console.log("Error sending your request" +"\n" + error));
 
     console.log(formData)
 })
