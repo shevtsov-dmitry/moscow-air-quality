7277fb84ad8f29dc53b9fb81e94513fd66824dad
shado <shadowstakeus28@gmail.com>
2023-03-12 15:48:41 +0300

    Проблема с кодировкой решена. Файл был сломан.

--- air-pollution-map-project-frontend/script/admin_page_script.js
+++ air-pollution-map-project-frontend/script/admin_page_script.js
@@ -5,15 +5,23 @@ const submitUploadBtn = document.querySelector(".submitUploadBtn");
 submitUploadBtn.addEventListener("click", () => {
     try{
         const file = CSVFile.files[0];
-        const FileObject = new FormData();
-        FileObject.append('file',file) // ???
+        //const FileObject = new FormData();
+        //FileObject.append('file',file) // ???
         
         const url = 'http://localhost:8080/uploadCSV';
 
-        fetch(url,{
-            method:'POST',
-            body: JSON.stringify(FileObject)
-        })
+        fetch(url, {
+            method: 'POST',
+            body: file,
+            headers: {
+                'Content-Type': 'text/csv'
+              },
+          })
+        //   .then((response) =>{
+        //     let responseTXT = document.querySelector(".temptext")
+        //     responseTXT.textContent(response)
+        //     return response.json();
+        //   })
     }
     catch(error){console.log("Произошла ошибка: " + error)};
 })
