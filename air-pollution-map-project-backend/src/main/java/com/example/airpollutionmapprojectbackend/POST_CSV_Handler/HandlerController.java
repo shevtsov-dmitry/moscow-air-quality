c4756b9d5288b4a4bf8f5629dae3b80a9d3d96b8
shado <shadowstakeus28@gmail.com>
2023-03-12 15:49:04 +0300

    Проблема с кодировкой решена. Файл был сломан.

--- air-pollution-map-project-backend/src/main/java/com/example/airpollutionmapprojectbackend/POST_CSV_Handler/HandlerController.java
+++ air-pollution-map-project-backend/src/main/java/com/example/airpollutionmapprojectbackend/POST_CSV_Handler/HandlerController.java
@@ -1,31 +1,29 @@
 package com.example.airpollutionmapprojectbackend.POST_CSV_Handler;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.opencsv.CSVReader;
 import com.opencsv.exceptions.CsvException;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.multipart.MultipartFile;
 
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.LinkedList;
-import java.util.List;
+import java.util.*;
 
 @CrossOrigin(origins = "http://localhost:3000", methods = { RequestMethod.GET, RequestMethod.POST })
 @RestController
 public class HandlerController {
     public static List<String[]> list = new ArrayList<>();
+    public static String mutual;
     @PostMapping("/uploadCSV")
-    public void uploadCSV(@RequestBody byte[] bytes) throws IOException, CsvException {
+    public String uploadCSV(@RequestBody byte[] bytes) throws IOException, CsvException {
         var inputStream = new ByteArrayInputStream(bytes);
         var csvReader = new CSVReader(new InputStreamReader(inputStream));
         list = csvReader.readAll();
+        return list.toString();
+
     }
-    @GetMapping("/uploadCSV")
+    @GetMapping(value = "/uploadCSV")
     public String showBytesOnScreen(){
         var sb = new StringBuilder();
         for (String[] s:
