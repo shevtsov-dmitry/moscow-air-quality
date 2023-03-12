a509d21b60e778dd953458792eace8c100735b96
shado <shadowstakeus28@gmail.com>
2023-03-12 23:49:12 +0300

    Синхронизация. чиню git ignore

---
+++ air-pollution-map-project-frontend/script/main.js
@@ -0,0 +1,17 @@
+import '../css/style.css';
+import {Map, View} from '../node_modules/ol';
+import TileLayer from '../node_modules/ol/layer/Tile';
+import OSM from '../node_modules/ol/source/OSM';
+
+const map = new Map({
+  target: 'map',
+  layers: [
+    new TileLayer({
+      source: new OSM()
+    })
+  ],
+  view: new View({
+    center: [0, 0],
+    zoom: 2
+  })
+});
