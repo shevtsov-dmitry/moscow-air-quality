43971094e2ae581785691c4e7308766db75e07f5
shado <shadowstakeus28@gmail.com>
2023-03-13 18:46:52 +0300

    deleted thingfs

--- air-pollution-map-project-backend/src/main/java/com/example/airpollutionmapprojectbackend/POST_CSV_Handler/HandlerRepository.java
+++
@@ -1,6 +0,0 @@
-package com.example.airpollutionmapprojectbackend.POST_CSV_Handler;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface HandlerRepository extends JpaRepository<Handler,Long> {
-}
--- air-pollution-map-project-backend/src/main/java/com/example/airpollutionmapprojectbackend/admin/AdminRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.airpollutionmapprojectbackend.admin;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface AdminRepository extends JpaRepository <Admin, Long> {
-    
-}
