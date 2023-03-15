f978f7a85b091da2b0662ac97150f963a58034c9
shado <shadowstakeus28@gmail.com>
2023-03-15 16:25:33 +0300

    Полировка кода. Исправил Handler и SQLScriptImportCSVToTable классы так же удалил случайно созданный ранее файл

--- air-pollution-map-project-backend/src/main/java/com/example/airpollutionmapprojectbackend/POST_CSV_Handler/Handler.java
+++ air-pollution-map-project-backend/src/main/java/com/example/airpollutionmapprojectbackend/POST_CSV_Handler/Handler.java
@@ -11,7 +11,7 @@ public class Handler {
     private  Long id;
     private  String date;
     private  long global_id;
-    private  String AdmArea;
+    private  String adm_area;
     private  String District;
     private  double Longitude;
     private  double Latitude;
@@ -21,12 +21,12 @@ public class Handler {
     public Handler() {
     }
 
-    public Handler(Long id, String date, long global_id, String admArea, String district,
+    public Handler(Long id, String date, long global_id, String adm_area, String district,
                    double longitude, double latitude, String location, String results) {
         this.id = id;
         this.date = date;
         this.global_id = global_id;
-        AdmArea = admArea;
+        this.adm_area = adm_area;
         District = district;
         Longitude = longitude;
         Latitude = latitude;
@@ -34,15 +34,102 @@ public class Handler {
         Results = results;
     }
 
-    public Handler(String date, long global_id, String admArea, String district,
+    public Handler(String date, long global_id, String adm_area, String district,
                    double longitude, double latitude, String location, String results) {
         this.date = date;
         this.global_id = global_id;
-        AdmArea = admArea;
+        this.adm_area = adm_area;
         District = district;
         Longitude = longitude;
         Latitude = latitude;
         Location = location;
         Results = results;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public long getGlobal_id() {
+        return global_id;
+    }
+
+    public void setGlobal_id(long global_id) {
+        this.global_id = global_id;
+    }
+
+    public String getAdm_area() {
+        return adm_area;
+    }
+
+    public void setAdm_area(String adm_area) {
+        this.adm_area = adm_area;
+    }
+
+    public String getDistrict() {
+        return District;
+    }
+
+    public void setDistrict(String district) {
+        District = district;
+    }
+
+    public double getLongitude() {
+        return Longitude;
+    }
+
+    public void setLongitude(double longitude) {
+        Longitude = longitude;
+    }
+
+    public double getLatitude() {
+        return Latitude;
+    }
+
+    public void setLatitude(double latitude) {
+        Latitude = latitude;
+    }
+
+    public String getLocation() {
+        return Location;
+    }
+
+    public void setLocation(String location) {
+        Location = location;
+    }
+
+    public String getResults() {
+        return Results;
+    }
+
+    public void setResults(String results) {
+        Results = results;
+    }
+
+    @Override
+    public String toString() {
+        return "Handler{" +
+                "id=" + id +
+                ", date='" + date + '\'' +
+                ", global_id=" + global_id +
+                ", adm_area='" + adm_area + '\'' +
+                ", District='" + District + '\'' +
+                ", Longitude=" + Longitude +
+                ", Latitude=" + Latitude +
+                ", Location='" + Location + '\'' +
+                ", Results='" + Results + '\'' +
+                '}';
     }
 }
