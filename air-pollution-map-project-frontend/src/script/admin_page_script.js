e0cb0a8ddf84e865ddba045edf892f99abfb5060
shado <shadowstakeus28@gmail.com>
2023-03-21 20:09:10 +0300

    Настроил hadsontable. теперь нужно использовать свои данные

--- air-pollution-map-project-frontend/src/script/admin_page_script.js
+++ air-pollution-map-project-frontend/src/script/admin_page_script.js
@@ -1,3 +1,7 @@
+// import '../node_modules/pikaday';
+// import Handsontable from '../node_modules/handsontable';
+import '../node_modules/handsontable/dist/handsontable.full.min.css';
+
 //const CSVFile = document.querySelector(".CSVfile");
 const CSVFile = document.querySelector('input[type="file"]');
 const submitUploadBtn = document.querySelector(".submitUploadBtn");
@@ -20,17 +24,40 @@ submitUploadBtn.addEventListener("click", () => {
     }
     catch(error){console.log("Произошла ошибка: " + error)};
 })
-// display incomming text from request to database
+let CSVtable;
+// get incomming JSON table from request to database
 const temptext = document.querySelector(".temptext")
 const url = "http://localhost:8080/dataTableToWebsite"
 function displayIncomingText(url, textBlock){
     fetch(url)
     .then(response => response.json())
     .then(data => {
-        textBlock.textContent = JSON.stringify(data); // do something with data
+        textBlock.innerHTML =JSON.stringify(data); // do something with data
+        CSVtable = data;
     })
     .catch(error =>{
         textBlock.textContent = `Something went wrong: ${error}`
     })
 }
 displayIncomingText(url,temptext)
+// display table with handsontable API
+
+let HTtable = document.querySelector('.HTtable')
+
+var data = [
+    { id: 1, name: 'John', age: 25 },
+    { id: 2, name: 'Mary', age: 30 },
+    { id: 3, name: 'Peter', age: 40 }
+  ];
+  
+  // Create Handsontable instance and fill it with data from JSON object
+  let hot = new Handsontable(HTtable, {
+    data: data,
+    colHeaders: ['ID', 'Name', 'Age'],
+    columns: [
+      { data: 'id' },
+      { data: 'name' },
+      { data: 'age' }
+    ],
+    licenseKey: 'non-commercial-and-evaluation'
+  });
