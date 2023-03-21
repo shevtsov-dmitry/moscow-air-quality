b9167322e9e5eea0bcc4ae3801dd643d4c5ef993
shado <shadowstakeus28@gmail.com>
2023-03-21 21:33:10 +0300

    Успешно получилось отобразить входящие данные из базы данных

--- air-pollution-map-project-frontend/src/script/admin_page_script_display_table.js
+++ air-pollution-map-project-frontend/src/script/admin_page_script_display_table.js
@@ -1,62 +1,42 @@
 
-let CSVtable;
-// get incomming JSON table from request to database
+let CSVtable =[];
+// get incomming JSON table with request from database
 const temptext = document.querySelector(".temptext")
 const url = "http://localhost:8080/dataTableToWebsite"
-function displayIncomingText(url){
-    return fetch(url)
+
+function getCsvResponse(url){
+    fetch(url)
     .then(response => response.json())
-    .then(data => {
-        // textBlock.innerHTML = JSON.stringify(data); // do something with data
-        return data;
+    .then(data => { // main part of script. using written functions
+        fillTable(data)
     })
     .catch(error =>{
         textBlock.textContent = `Something went wrong: ${error}`
     })
 }
-CSVtable = displayIncomingText(url)
+
+getCsvResponse(url)
 
 // display table with handsontable API
 
-let HTtable = document.querySelector('.HTtable')
-
-let data = [
-    { id: 1, name: 'John', age: 25 },
-    { id: 2, name: 'Mary', age: 30 },
-    { id: 3, name: 'Peter', age: 40 }
-  ]
-  
-  // Create Handsontable instance and fill it with data from JSON object
-  let hot = new Handsontable(HTtable, {
-    data: data,
-    colHeaders: ['ID', 'Name', 'Age'],
-    columns: [
-      { data: 'id' },
-      { data: 'name' },
-      { data: 'age' }
-    ],
-    licenseKey: 'non-commercial-and-evaluation'
-  });
-
-  // my csv data
-  const csvDataDiv = document.querySelector('.csvdata')
-  
-  let csvData = new Handsontable(csvDataDiv,{
-    data: CSVtable,
-    colHeaders: ['1','2','3','4','5','6','7','8','9','10'],
-    columns: [{ data: 'id' },
-    { CSVtable: 'id' },
-    { CSVtable: 'date' },
-    { CSVtable: 'global_id' },
-    { CSVtable: 'adm_area' },
-    { CSVtable: 'location' },
-    { CSVtable: 'district' },
-    { CSVtable: 'logitude' },
-    { CSVtable: 'latitude' },
-    { CSVtable: 'results' }
-    ],
-    licenseKey: 'non-commercial-and-evaluation'
-  })
-
-  const displayJSON = document.querySelector('.displayJSON')
-  displayJSON.innerHTML = JSON.stringify(CSVtable)
+function fillTable(CSVtable){
+    const csvDataDiv = document.querySelector('.csvdata')
+    let hot = new Handsontable(csvDataDiv,{
+        data: CSVtable,
+        colHeaders: ['1','2','3','4','5','6','7','8','9'],
+        columns: [
+        { data: 'id' },
+        { data: 'date' },
+        { data: 'global_id' },
+        { data: 'adm_area' },
+        { data: 'location' },
+        { data: 'district' },
+        { data: 'longitude' },
+        { data: 'latitude' },
+        { data: 'results' }
+        ],
+        licenseKey: 'non-commercial-and-evaluation'
+      })
+}
+//   const displayJSON = document.querySelector('.displayJSON')
+//   displayJSON.innerHTML = JSON.stringify(CSVtable)
