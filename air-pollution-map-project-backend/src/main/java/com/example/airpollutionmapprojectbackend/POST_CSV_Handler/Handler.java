98548f5aa5c2c83c9c7dac958442ef3cb745fc80
shado <shadowstakeus28@gmail.com>
2023-03-26 14:26:50 +0300

    Переопределил Handler класс для формата новой таблицы.

--- air-pollution-map-project-backend/src/main/java/com/example/airpollutionmapprojectbackend/POST_CSV_Handler/Handler.java
+++ air-pollution-map-project-backend/src/main/java/com/example/airpollutionmapprojectbackend/POST_CSV_Handler/Handler.java
@@ -4,62 +4,49 @@ import jakarta.persistence.*;
 
 import java.sql.Date;
 @Entity
-@Table(name ="air_pollution_csv_table")
+@Table(name ="air_pollution_data")
 public class Handler {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private  Long id;
-    private  Date date;
-    private  long global_id;
-    private  String adm_area;
-    private  String District;
-    private  double Longitude;
-    private  double Latitude;
-    private  String Location;
-    private  String Results;
+    private long id;
+    private long global_id;
+    private String station_name;
+    private double latitude;
+    private double longitude;
+    private String Surveillance_zone_characteristics;
+    private String adm_area;
+    private String district;
+    private String location;
+    private String parameter;
+    private double monthly_average;
+    private double monthly_average_pdkss;
 
     public Handler() {
     }
 
-    public Handler(Long id, Date date, long global_id, String adm_area, String district,
-                   double longitude, double latitude, String location, String results) {
+    public Handler(long id, long global_id, String station_name, double latitude, double longitude,
+                   String surveillance_zone_characteristics, String adm_area, String district,
+                   String location, String parameter, double monthly_average, double monthly_average_pdkss) {
         this.id = id;
-        this.date = date;
         this.global_id = global_id;
+        this.station_name = station_name;
+        this.latitude = latitude;
+        this.longitude = longitude;
+        Surveillance_zone_characteristics = surveillance_zone_characteristics;
         this.adm_area = adm_area;
-        District = district;
-        Longitude = longitude;
-        Latitude = latitude;
-        Location = location;
-        Results = results;
+        this.district = district;
+        this.location = location;
+        this.parameter = parameter;
+        this.monthly_average = monthly_average;
+        this.monthly_average_pdkss = monthly_average_pdkss;
     }
 
-    public Handler(Date date, long global_id, String adm_area, String district,
-                   double longitude, double latitude, String location, String results) {
-        this.date = date;
-        this.global_id = global_id;
-        this.adm_area = adm_area;
-        District = district;
-        Longitude = longitude;
-        Latitude = latitude;
-        Location = location;
-        Results = results;
-    }
-
-    public Long getId() {
+    public long getId() {
         return id;
     }
 
-    public void setId(Long id) {
+    public void setId(long id) {
         this.id = id;
-    }
-
-    public Date getDate() {
-        return date;
-    }
-
-    public void setDate(Date date) {
-        this.date = date;
     }
 
     public long getGlobal_id() {
@@ -68,6 +55,38 @@ public class Handler {
 
     public void setGlobal_id(long global_id) {
         this.global_id = global_id;
+    }
+
+    public String getStation_name() {
+        return station_name;
+    }
+
+    public void setStation_name(String station_name) {
+        this.station_name = station_name;
+    }
+
+    public double getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(double latitude) {
+        this.latitude = latitude;
+    }
+
+    public double getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(double longitude) {
+        this.longitude = longitude;
+    }
+
+    public String getSurveillance_zone_characteristics() {
+        return Surveillance_zone_characteristics;
+    }
+
+    public void setSurveillance_zone_characteristics(String surveillance_zone_characteristics) {
+        Surveillance_zone_characteristics = surveillance_zone_characteristics;
     }
 
     public String getAdm_area() {
@@ -79,57 +98,60 @@ public class Handler {
     }
 
     public String getDistrict() {
-        return District;
+        return district;
     }
 
     public void setDistrict(String district) {
-        District = district;
-    }
-
-    public double getLongitude() {
-        return Longitude;
-    }
-
-    public void setLongitude(double longitude) {
-        Longitude = longitude;
-    }
-
-    public double getLatitude() {
-        return Latitude;
-    }
-
-    public void setLatitude(double latitude) {
-        Latitude = latitude;
+        this.district = district;
     }
 
     public String getLocation() {
-        return Location;
+        return location;
     }
 
     public void setLocation(String location) {
-        Location = location;
+        this.location = location;
     }
 
-    public String getResults() {
-        return Results;
+    public String getParameter() {
+        return parameter;
     }
 
-    public void setResults(String results) {
-        Results = results;
+    public void setParameter(String parameter) {
+        this.parameter = parameter;
+    }
+
+    public double getMonthly_average() {
+        return monthly_average;
+    }
+
+    public void setMonthly_average(double monthly_average) {
+        this.monthly_average = monthly_average;
+    }
+
+    public double getMonthly_average_pdkss() {
+        return monthly_average_pdkss;
+    }
+
+    public void setMonthly_average_pdkss(double monthly_average_pdkss) {
+        this.monthly_average_pdkss = monthly_average_pdkss;
     }
 
     @Override
     public String toString() {
         return "Handler{" +
                 "id=" + id +
-                ", date=" + date +
                 ", global_id=" + global_id +
+                ", station_name='" + station_name + '\'' +
+                ", latitude=" + latitude +
+                ", longitude=" + longitude +
+                ", Surveillance_zone_characteristics='" + Surveillance_zone_characteristics + '\'' +
                 ", adm_area='" + adm_area + '\'' +
-                ", District='" + District + '\'' +
-                ", Longitude=" + Longitude +
-                ", Latitude=" + Latitude +
-                ", Location='" + Location + '\'' +
-                ", Results='" + Results + '\'' +
+                ", district='" + district + '\'' +
+                ", location='" + location + '\'' +
+                ", parameter='" + parameter + '\'' +
+                ", monthly_average=" + monthly_average +
+                ", monthly_average_pdkss=" + monthly_average_pdkss +
                 '}';
     }
 }
