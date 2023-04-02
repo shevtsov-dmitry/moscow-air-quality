02143bc86390ac38ca9fab7c12aa951a61fba477
shado <shadowstakeus28@gmail.com>
2023-04-02 22:31:13 +0300

    Остановил работу на получении колонки из таблицы с помощью новой async fn

--- air-pollution-map-project-frontend/src/script/display_table.js
+++ air-pollution-map-project-frontend/src/script/display_table.js
@@ -1,5 +1,12 @@
-import Handsontable from '../node_modules/handsontable'
-import '../node_modules/handsontable/dist/handsontable.full.min.css'
+// table initialization
+// configuration optimized in async function - fillTable()
+const csvDataDiv = document.querySelector('.csvdata')
+let hot = new Handsontable(csvDataDiv,{
+    data: [[]],
+    colHeaders: true,
+    columns: true,
+    licenseKey: 'non-commercial-and-evaluation'
+})
 
 const url = "http://localhost:8080/dataTableToWebsite"
 getCsvResponse(url)
@@ -21,38 +28,78 @@ function getCsvResponse(url){
 // get columns names from database
 // parent async fn
 async function getColumnNames(){
-    const url = "http://localhost:8080/getColumnNames";
-    const response = await fetch(url)
-    data = await response.json()
-    return data
+    try{
+        const url = "http://localhost:8080/getColumnNames";
+        const response = await fetch(url)
+        let data = await response.json()
+        return data
+    }
+    catch(error){
+        console.log("Failed to obtain column names, because: " + error)
+    }
+    
 }
 // display table with handsontable API
 // this function will start working only after parent getColumnNames()
 async function fillTable(CSVtable){
-    let list = '' // string of column's names
-    const data = await getColumnNames(); // getting incomming data from function
+    try{
+        let list = '' // string of column's names
+        const data = await getColumnNames(); // getting incomming data from function
 
-    // fixing string to appropriate format
-    list = JSON.stringify(data)
-    list = list.substring(1,list.length - 1)
-    list = list.replaceAll('"','')
+        // fixing string to appropriate format
+        list = JSON.stringify(data)
+        list = list.substring(1,list.length - 1)
+        list = list.replaceAll('"','')
 
-    // adding all strings into array
-    arr = list.split(',')
+        // adding all strings into array
+        let arr = list.split(',')
 
-    // filling columns array with values 
-    let columns = arr.map(element => {
-        return {data: element.toLowerCase()} // lowercase is essential!
-      })
+        // filling columns array with values 
+        let columns = arr.map(element => {
+            return {data: element.toLowerCase()} // lowercase is essential!
+        })
 
-    // table config
-    const csvDataDiv = document.querySelector('.csvdata')
-    let hot = new Handsontable(csvDataDiv,{
-        data: CSVtable,
-        colHeaders: arr,
-        columns: columns,
-        licenseKey: 'non-commercial-and-evaluation'
-    })
-    console.log(hot.getColumnDataAtCol(1))
+        // table config
+        hot = new Handsontable(csvDataDiv,{
+            data: CSVtable,
+            colHeaders: arr,
+            columns: columns,
+            licenseKey: 'non-commercial-and-evaluation'
+        })
+
+        // let dates = hot.getDataAtCol(1)
+        // for (let i = 0; i < dates.length; i++) {
+        //     const dateObj = new Date(dates[i]);
+        //     const day = dateObj.getDate();
+        //     const month = dateObj.getMonth() + 1;
+        //     const year = dateObj.getFullYear();
+        //     const formattedDate = `${day}.${month}.${year}`;
+        //     console.log(formattedDate);
+        //   }
+    }
+    catch(error){
+        console.log(`Something went wrong in fillTable function: ${error}`);
+    }
 }
 
+async function awaitMe(){
+    let data = await hot.getDataAtCol(1)
+    return data
+}
+
+async function main() {
+    try {
+      await fillTable()
+      let arrWin = await awaitMe()
+      console.log(arrWin)
+    } catch (error) {
+      console.log(`Error in main function: ${error}`)
+    }
+  }
+  
+  main()
+
+let pressThis = document.querySelector(".temptext")
+pressThis.addEventListener('click',()=>{
+    console.log(arrWin)
+})
