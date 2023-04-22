306776b5f63f9df8c88ffb9b185cfb3138c9895f
shado <shadowstakeus28@gmail.com>
2023-04-22 14:48:27 +0300

    feat, style: finish with form choose variant

--- air-pollution-map-project-frontend/src/script/admin_page/small_features.js
+++ air-pollution-map-project-frontend/src/script/admin_page/small_features.js
@@ -5,18 +5,73 @@ show_buttons_btn.addEventListener('click', ()=>{
     selectors.style.display = 'flex'
 })
 
+// buttons
+const btn_everything = document.querySelector(".btn_everything")
+const btn_ID = document.querySelector(".btn_ID")
+const btn_station_name = document.querySelector(".btn_station_name")
+const btn_parameter = document.querySelector(".btn_parameter")
 
-const upload_btn = document.querySelector('.submitUploadBtn')
-const loading_gif = document.querySelector('.loading-gif')
+// functions of buttons
 
-// * show gif when upload file
-upload_btn.addEventListener('click', ()=>{
-    loading_gif.style.display = 'initial'
+btn_everything.addEventListener('click',()=>{
+    hideCloseSign()
+    hideForm()
+    form_filled_with_variants.innerHTML=""
+})
+btn_ID.addEventListener('click',()=>{
+    hideCloseSign()
+    showForm()
+})
+btn_station_name.addEventListener('click',()=>{
+    showCloseSign()
+    showForm()
+})
+btn_parameter.addEventListener('click',()=>{
+    showCloseSign()
+    showForm()
 })
 
-// * close div when hover off the edge
-const list_of_elements = document.querySelector('.form-filled-with-variants')
+const submit_btn = document.querySelector('.submitUploadBtn')
+const loading_gif = document.querySelector('.loading-gif')
 
-// list_of_elements.addEventListener('mouseout', ()=> {
-//     list_of_elements.innerHTML = ""
-// })
+// * show gif when upload file and hide the panel where user inputted value
+submit_btn.addEventListener('click', ()=>{
+    loading_gif.style.display = 'initial'
+    setTimeout(()=>{
+        form_filled_with_variants.innerHTML = ''
+    },400)
+})
+
+// * close sign actions
+const close_sign = document.querySelector('.close-sign')
+close_sign.addEventListener('click',()=>{
+    setTimeout(()=>{
+        hideCloseSign()
+        hideForm()
+        form_filled_with_variants.innerHTML = ''
+    },200)
+})
+
+function showCloseSign(){
+    close_sign.style.display = 'initial'
+}
+function hideCloseSign(){
+    close_sign.style.display = 'none'
+}
+
+const form_filled_with_variants = document.querySelector('.form-filled-with-variants')
+function showForm(){
+    form_filled_with_variants.style.display = 'initial'
+}
+function hideForm(){
+    form_filled_with_variants.style.display = 'none'
+}
+
+form_filled_with_variants.addEventListener('click',()=>{
+    setTimeout(()=>{
+        hideCloseSign()
+        form_filled_with_variants.innerHTML = ""
+        hideForm()
+    },500)
+})
+
