d7e468a5986e8faa57363b270683c654b5fae519
shado <shadowstakeus28@gmail.com>
2023-06-13 14:19:03 +0300

    refactor: vite specify IP

--- air-pollution-map-project-frontend/src/vite.config.js
+++ air-pollution-map-project-frontend/src/vite.config.js
@@ -1,27 +1,11 @@
 import { defineConfig } from 'vite';
 export default defineConfig({
-  // entry: 'node-modules-openlayers-vite',
-  // resolve: {
-  //   alias: {
-  //     // create an alias for the renamed node_modules folder
-  //     'openlayers': path.resolve(__dirname, 'node-modules-openlayers-vite')
-  //   }
-  // },
   entry: 'package.json',
-  //root: '../air-pollution-map-project-frontend/script',
-  // root: path.resolve(__dirname, '../air-pollution-map-project-frontend'),
-  // base: './',
   build: {
     outDir: 'dist',
   },
-  server: {
+  server: { // Настройка IP
     port: 3000,
+    host: 'localhost',
   },
-  // optimizeDeps: {
-  //   include: [
-  //     // 'ol/Map.js',
-  //     // 'ol/View.js',
-  //     './main.js',
-  //   ],
-  // },
 });
