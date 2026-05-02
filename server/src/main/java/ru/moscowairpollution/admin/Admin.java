f57ee270448fb89da89344b9930094e40180f523
shevtsov-dmitry <shadowstakeus28@gmail.com>
2026-05-02 15:59:21 +0300

    refactor: use lombok for Admin class

--- server/src/main/java/ru/moscowairpollution/admin/Admin.java
+++ server/src/main/java/ru/moscowairpollution/admin/Admin.java
@@ -3,59 +3,19 @@ package ru.moscowairpollution.admin;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "admins")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Admin {
     @Id
     private long id;
     private String login;
     private String password;
 
-    public Admin() {
-    }
-
-    public Admin(long id, String login, String password) {
-        this.id = id;
-        this.login = login;
-        this.password = password;
-    }
-
-    public Admin(String login, String password) {
-        this.login = login;
-        this.password = password;
-    }
-
-    public long getId() {
-        return id;
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    public String getLogin() {
-        return login;
-    }
-
-    public void setLogin(String login) {
-        this.login = login;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    @Override
-    public String toString() {
-        return "Admin{" +
-                "id=" + id +
-                ", login='" + login + '\'' +
-                ", password='" + password + '\'' +
-                '}';
-    }
 }
