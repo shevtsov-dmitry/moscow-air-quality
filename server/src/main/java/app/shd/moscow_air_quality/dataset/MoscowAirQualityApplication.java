8c9713a61a844492c901fc2f4693d29bcb4f1993
shevtsov-dmitry <shadowstakeus28@gmail.com>
2026-05-02 16:02:56 +0300

    refactor: use new modern spring boot runtime with working maven wrapper

---
+++ server/src/main/java/app/shd/moscow_air_quality/dataset/MoscowAirQualityApplication.java
@@ -0,0 +1,13 @@
+package app.shd.moscow_air_quality;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoscowAirQualityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoscowAirQualityApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/app/shd/moscow_air_quality/MoscowAirQualityApplicationTests.java
@@ -0,0 +1,13 @@
+package app.shd.moscow_air_quality;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoscowAirQualityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
