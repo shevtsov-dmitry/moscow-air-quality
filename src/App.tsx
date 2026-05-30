6ff858fd34fb769b0c03fecc4029b55dbc0fd538
shevtsov-dmitry <shadowstakeus28@gmail.com>
2026-05-30 11:33:48 +0300

    refactor: transfer fundamental core of a website

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,33 @@
-import "./index.css";
+// import "./index.css";
 
 export function App() {
   return (
     <main>
-      <div class="text-is-not-uploaded">
+      <div className="text-is-not-uploaded">
         <h1>Данные не загружены</h1>
       </div>
 
-      <div class="authorization-icon">
-        <a href="html/authorization.html"><img src="static/authorization_icon.png" alt="log in" /></a>
+      <div className="authorization-icon">
+        {/* TODO move with react  */}
+        {/* <a href="html/authorization.html"> */}
+        <img src="static/authorization_icon.png" alt="log in" />
+        {/* </a> */}
       </div>
 
-      <div class="choose-date-icon-div">
-        <img src="static/calendar.png" class="btn-img-to-show-date-choose" alt="" srcset="" />
+      <div className="choose-date-icon-div">
+        <img src="./assets/images/calendar.png" className="btn-img-to-show-date-choose" alt="" srcset="" />
       </div>
-      <div class="list-of-dates-div">
-        <ul class="choose-year" />
-        <ul class="choose-month" />
+      <div className="list-of-dates-div">
+        <ul className="choose-year" />
+        <ul className="choose-month" />
       </div>
 
-      <div class="select-container">
-        <h1 class="select-close-sign">X</h1>
-        <ul class="select-ul" />
-        <div class="emote-and-color-block">
-          <div class="color-block">
-            <div class="image-block" />
+      <div className="select-container">
+        <h1 className="select-close-sign">X</h1>
+        <ul className="select-ul" />
+        <div className="emote-and-color-block">
+          <div className="color-block">
+            <div className="image-block" />
           </div>
         </div>
       </div>
